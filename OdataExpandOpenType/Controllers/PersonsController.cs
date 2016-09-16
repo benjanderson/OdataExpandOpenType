cbc107c1222c459b2c0d3f65c0804883437eca7b
Ben Anderson <ben.anderson@healthcatalyst.com>
2016-09-16 16:26:59 -0600

    ripout open type

--- OdataExpandOpenType/Controllers/PersonsController.cs
+++ OdataExpandOpenType/Controllers/PersonsController.cs
@@ -1,41 +1,41 @@
 ﻿namespace OdataExpandOpenType.Controllers
 {
-  using System.Data.Entity;
-  using System.Linq;
-	using System.Threading.Tasks;
-  using System.Web.Http;
-	using System.Web.Http.Description;
-	using System.Web.OData;
-	using System.Web.OData.Routing;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.Http.Description;
+    using System.Web.OData;
+    using System.Web.OData.Routing;
 
-	[EnableQuery]
-  public class PersonsController : ODataController
-  {
-    public PersonsController()
+    [EnableQuery]
+    public class PersonsController : ODataController
     {
-      this.dbContext = new PersonContext();
+        public PersonsController()
+        {
+            this.dbContext = new PersonContext();
+        }
+
+        private readonly PersonContext dbContext;
+
+        [HttpGet]
+        [ResponseType(typeof(IQueryable<Person>))]
+        [ODataRoute("Persons")]
+        [Route("api/Persons")]
+        public IHttpActionResult Get()
+        {
+            return this.Ok(this.dbContext.Persons);
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(Person))]
+        [ODataRoute("Persons")]
+        [Route("api/Persons")]
+        public async Task<IHttpActionResult> Post([FromBody] Person person)
+        {
+            this.dbContext.Persons.Add(person);
+            await this.dbContext.SaveChangesAsync();
+            return this.Ok(person);
+        }
     }
-
-    private readonly PersonContext dbContext;
-
-	  [HttpGet]
-		[ResponseType(typeof(IQueryable<Person>))]
-		[ODataRoute("Persons")]
-	  [Route("api/Persons")]
-	  public IHttpActionResult Get()
-	  {
-		  return this.Ok(this.dbContext.Persons.Include(p => p.Attributes));
-	  }
-
-	  [HttpPost]
-		[ResponseType(typeof(Person))]
-    [ODataRoute("Persons")]
-    [Route("api/Persons")]
-    public async Task<IHttpActionResult> Post([FromBody] Person person)
-    {
-      this.dbContext.Persons.Add(person);
-      await this.dbContext.SaveChangesAsync();
-      return this.Ok(person);
-    }
-  } 
 }
