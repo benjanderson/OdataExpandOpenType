610b10ca2cac40cf812e7f4082fcdd3cde928739
Ben Anderson <ben.anderson@healthcatalyst.com>
2016-09-23 17:54:54 -0600

    asdf

--- OdataExpandOpenType/Controllers/PersonsController.cs
+++ OdataExpandOpenType/Controllers/PersonsController.cs
@@ -1,10 +1,13 @@
 ﻿namespace OdataExpandOpenType.Controllers
 {
+    using System;
     using System.Data.Entity;
     using System.Linq;
+    using System.Reflection;
     using System.Threading.Tasks;
     using System.Web.Http;
     using System.Web.Http.Description;
+    using System.Web.Http.OData.Query;
     using System.Web.OData;
     using System.Web.OData.Routing;
 
@@ -22,9 +25,21 @@
         [ResponseType(typeof(IQueryable<Person>))]
         [ODataRoute("Persons")]
         [Route("api/Persons")]
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(ODataQueryOptions<Person> options)
         {
-            return this.Ok(this.dbContext.Persons.Include(a => a.Attributes));
+            var persons = this.dbContext.Persons;
+            
+            return this.Ok(persons);
+            //            var expand =  ("Attributes," + options.RawValues.Expand);
+            //            var expandQueryOption = new SelectExpandQueryOption(options.RawValues.Select, expand, options.Context);
+            //            var childProp = typeof(ODataQueryOptions<Person>).GetProperty(nameof(options.SelectExpand));
+            //            var parentProp = childProp.DeclaringType.GetProperty(nameof(options.SelectExpand));
+            //            parentProp.SetValue(options, expandQueryOption, null);
+            //
+            //            options.RawValues.SetPrivatePropertyValue(nameof(options.RawValues.Expand), expand);
+            //
+            //            IQueryable result = options.ApplyTo(persons);
+            //            return this.Ok(result);
         }
 
         [HttpPost]
@@ -37,6 +52,86 @@
             this.dbContext.Persons.Add(person);
             await this.dbContext.SaveChangesAsync();
             return this.Ok(person);
+        }
+    }
+
+    public static class PrivateHelper
+    {
+        /// <summary>
+        /// Returns a _private_ Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is returned</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <returns>PropertyValue</returns>
+        public static T GetPrivatePropertyValue<T>(this object obj, string propName)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            PropertyInfo pi = obj.GetType().GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            if (pi == null) throw new ArgumentOutOfRangeException("propName", string.Format("Property {0} was not found in Type {1}", propName, obj.GetType().FullName));
+            return (T)pi.GetValue(obj, null);
+        }
+
+        /// <summary>
+        /// Returns a private Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is returned</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <returns>PropertyValue</returns>
+        public static T GetPrivateFieldValue<T>(this object obj, string propName)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            Type t = obj.GetType();
+            FieldInfo fi = null;
+            while (fi == null && t != null)
+            {
+                fi = t.GetField(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                t = t.BaseType;
+            }
+            if (fi == null) throw new ArgumentOutOfRangeException("propName", string.Format("Field {0} was not found in Type {1}", propName, obj.GetType().FullName));
+            return (T)fi.GetValue(obj);
+        }
+
+        /// <summary>
+        /// Sets a _private_ Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is set</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <param name="val">Value to set.</param>
+        /// <returns>PropertyValue</returns>
+        public static void SetPrivatePropertyValue<T>(this object obj, string propName, T val)
+        {
+            Type t = obj.GetType();
+            if (t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
+                throw new ArgumentOutOfRangeException("propName", string.Format("Property {0} was not found in Type {1}", propName, obj.GetType().FullName));
+            t.InvokeMember(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance, null, obj, new object[] { val });
+        }
+
+        /// <summary>
+        /// Set a private Property Value on a given Object. Uses Reflection.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is returned</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <param name="val">the value to set</param>
+        /// <exception cref="ArgumentOutOfRangeException">if the Property is not found</exception>
+        public static void SetPrivateFieldValue<T>(this object obj, string propName, T val)
+        {
+            if (obj == null) throw new ArgumentNullException("obj");
+            Type t = obj.GetType();
+            FieldInfo fi = null;
+            while (fi == null && t != null)
+            {
+                fi = t.GetField(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                t = t.BaseType;
+            }
+            if (fi == null) throw new ArgumentOutOfRangeException("propName", string.Format("Field {0} was not found in Type {1}", propName, obj.GetType().FullName));
+            fi.SetValue(obj, val);
         }
     }
 }
